d6bc2183c484c89bcab2723cea0f86e52474ae96
haifa-hannan <haifarosea103@gmail.com>
2023-11-02 18:24:53 +0700

    up

--- lelang/models.py
+++ lelang/models.py
@@ -6,27 +6,27 @@ class Product(models.Model):
     productid = models.UUIDField(primary_key=True, default=uuid.uuid4)
     prodname = models.CharField(blank=True, null=True, max_length=150)
     desc = models.TextField(blank=True, null=True)
-    imgid = models.ImageField(blank=True, null=True)
+    img = models.ForeignKey('Image', on_delete=models.CASCADE,blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
-    status = models.CharField(blank=True, null=True, max_length=150)
+    # status = models.ForeignKey('Status', on_delete=models.CASCADE, blank=True, null=True)
+    statusid = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
         managed = True
         db_table = 'product'
-
-class Status(models.Model):
-    statusid = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    status = models.CharField(blank=True, null=True, max_length=150)
-
-    class Meta:
-        managed = True
-        db_table = 'status'
-
         
 class Image(models.Model):
     imgid = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    img = models.FileField(blank=True, null=True)
+    img = models.FileField(upload_to='images/')
 
     class Meta:
         managed = True
         db_table = 'image'
+
+class Status(models.Model):
+    statusid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    status = models.CharField(blank=True, null=True, max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'status'        
--- lelang/serializers.py
+++ lelang/serializers.py
@@ -5,3 +5,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
