c1b306c687401ada6a9f2de645cca416c4e4562b
haifa-hannan <haifarosea103@gmail.com>
2023-11-07 14:50:02 +0700

    typos

--- lelang/urls.py
+++ lelang/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("get-products/", views.GetProduct.as_view(), name="get-products"),
     path("add-products/", views.ProductViews.as_view(), name="add-products"),
     path("del-products/", views.DeleteProduct.as_view(), name="del-product"),
-    path("upd-products/", views.UpdateProduct.as_view(), name="upd-product"),
+    # path("upd-products/", views.UpdateProduct.as_view(), name="upd-product"),
     path("upd2-products/", views.UpdateProduct2.as_view(), name="upd2-product"),
 ]
